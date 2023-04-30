56543d893cd83c78acb167c04b41049425b171ed
eraybulut34 <abdullaheraybulut@hotmail.com>
2023-05-01 02:37:36 +0300

    init

---
+++ PostgreSqlDotnetCore/Models/User.cs
@@ -0,0 +1,10 @@
+namespace PostgreSqlDotnetCore.Models
+{
+    public class Usser
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
